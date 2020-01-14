1acd9005b17ea2eeaaf22b93fb3951acab59d399
Mikhail <mschee@physics.utoronto.ca>
2020-01-14 18:53:48 -0500

    Now plots 1D CD nicely

--- fourier_test.py
+++ fourier_test.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def make_subplot(hori, vert, data, fig, ax, h_label, v_label, title):
-    #im = ax.pcolormesh(data)
     im = ax.pcolormesh(hori, vert, data)
     ax.set_xlabel(h_label)
     ax.set_ylabel(v_label)
@@ -29,10 +28,9 @@ dz = (zf-z0)/nz
 t = np.linspace(t0, tf, nt)
 z = np.linspace(z0, zf, nz)
 tm, zm = np.meshgrid(t, z)
+# Total field
 y = A*np.cos(omega * tm - kz*zm) + B*np.cos(omega * tm + kz*zm)
-print('y ', y.shape)
-print('tm ', tm.shape)
-print('zm ', zm.shape)
+# Separate up and down analytically for confirmation that CD is working
 up = A*np.cos(omega * tm - kz*zm)
 dn = B*np.cos(omega * tm + kz*zm)
 
@@ -56,7 +54,6 @@ for i in range(ftm.shape[0]):
 
 # inverse fourier transform in time
 ifty = np.fft.ifft(fty, axis=1)
-print('ifty ', ifty.shape)
 #   ify is now a complex valued signal where ifty.real == y, or at least close enough
 
 ### Step 2
@@ -64,14 +61,9 @@ print('ifty ', ifty.shape)
 # fourier transform in spatial dimension (z)
 #   similar to ft in time, but switch dimensions around
 fzyp = np.fft.fft(ifty, axis=0)
-print('fzyp ', fzyp.shape)
 # find relevant wavenumbers
 wavn = np.fft.fftfreq(len(z), dz)
-print('wavn ', wavn.shape)
-print('t ', t.shape)
 tfz, fzm = np.meshgrid(t, wavn)
-print('fzm ', fzm.shape)
-print(fzm)
 # make a copy for the negative wave numbers
 fzyn = fzyp.copy()
 
@@ -79,15 +71,11 @@ fzyn = fzyp.copy()
 for i in range(fzm.shape[0]):
     for j in range(fzm.shape[1]):
         if fzm[i][j] > 0.0:
-            # for up, double values for positive wave numbers
-            fzyp[i][j] = fzyp[i][j] * 2.0
-            # for down, remove values for positive wave numbers
-            fzyn[i][j] = 0.0
-        else:
-            # for up, remove values for negative wave numbers
+            # for up, remove values for positive wave numbers
             fzyp[i][j] = 0.0
-            # for down, double values for negative wave numbers
-            fzyn[i][j] = fzyn[i][j] * 2.0
+        else:
+            # for down, remove values for negative wave numbers
+            fzyn[i][j] = 0.0
 
 # inverse fourier transform in space (z)
 ifzyp = np.fft.ifft(fzyp, axis=0)
@@ -98,10 +86,19 @@ up_field = ifzyp.real * np.exp(np.real(1j * ifzyp.imag))
 dn_field = ifzyn.real * np.exp(np.real(1j * ifzyn.imag))
 
 # plotting
-fig, ax = plt.subplots(3,1)
-make_subplot(tm, zm, y, fig, ax[0], r'$t$', r'$z$', 'OG')
-make_subplot(tm, zm, up_field, fig, ax[1], r'$t$', r'$z$', 'up')
-make_subplot(tm, zm, dn_field, fig, ax[2], r'$t$', r'$z$', 'down')
+rows = 2
+cols = 3
+fig, ax = plt.subplots(rows, cols)
+make_subplot(tm, zm, y, fig, ax[0][0], r'$t$', r'$z$', r'$A\cos(\omega t-k_z z)+B\cos(\omega t+k_z z)$')
+make_subplot(tm, zm, up, fig, ax[0][1], r'$t$', r'$z$', r'$A\cos(\omega t-k_z z)$')
+make_subplot(tm, zm, dn, fig, ax[0][2], r'$t$', r'$z$', r'$B\cos(\omega t+k_z z)$')
+make_subplot(tm, zm, up_field+dn_field, fig, ax[1][0], r'$t$', r'$z$', 'Up + Down')
+make_subplot(tm, zm, up_field, fig, ax[1][1], r'$t$', r'$z$', 'Up')
+make_subplot(tm, zm, dn_field, fig, ax[1][2], r'$t$', r'$z$', 'Down')
 
+# Set aspect ratio for figure
+size_factor = 3.0
+w, h = cols*size_factor, (rows+0.4)*size_factor
+plt.gcf().set_size_inches(w, h)
 plt.tight_layout()
 plt.show()
