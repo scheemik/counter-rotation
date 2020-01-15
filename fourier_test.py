f46ea71186416acd33afc910b1ef350978719169
Mikhail <mschee@physics.utoronto.ca>
2020-01-15 13:21:41 -0500

    Can now switch order of FT in t and z

--- fourier_test.py
+++ fourier_test.py
@@ -8,6 +8,51 @@ def make_subplot(hori, vert, data, fig, ax, h_label, v_label, title):
     ax.set_title(title)
     fig.colorbar(im, ax=ax)
 
+# fourier transform in time, filter negative freq's, inverse fourier transform
+def FT_in_time(t, z, data, dt):
+    # FT in time of the data (axis 1 is time)
+    ftd = np.fft.fft(data, axis=1)
+    # find relevant frequencies
+    freq = np.fft.fftfreq(len(t), dt)
+    f_grid, z_grid = np.meshgrid(freq, z)
+    # Filter out negative frequencies
+    for i in range(f_grid.shape[0]):
+        for j in range(f_grid.shape[1]):
+            if f_grid[i][j] < 0.0:
+                # Gets rid of negative freq's
+                ftd[i][j] = 0
+            else:
+                # Corrects for lost amplitude
+                ftd[i][j] = ftd[i][j] * 2.0
+    # inverse fourier transform in time of the data
+    iftd = np.fft.ifft(ftd, axis=1)
+    #   a complex valued signal where iftd.real == data, or close enough
+    return iftd
+
+# fourier transform in spatial dimension (z)
+#   similar to FT in time, but switch dimensions around
+def FT_in_space(t, z, data, dz):
+    # FT in space (z) of the data (axis 0 is z) for positive wave numbers
+    fzdp = np.fft.fft(data, axis=0)
+    # make a copy for the negative wave numbers
+    fzdn = fzdp.copy()
+    # find relevant wavenumbers
+    k_zs = np.fft.fftfreq(len(z), dz)
+    t_grid, k_grid = np.meshgrid(t, k_zs)
+    # Filter out one half of wavenumbers to separate up and down
+    for i in range(k_grid.shape[0]):
+        for j in range(k_grid.shape[1]):
+            if k_grid[i][j] > 0.0:
+                # for up, remove values for positive wave numbers
+                fzdp[i][j] = 0.0
+            else:
+                # for down, remove values for negative wave numbers
+                fzdn[i][j] = 0.0
+    # inverse fourier transform in space (z)
+    ifzdp = np.fft.ifft(fzdp, axis=0)
+    ifzdn = np.fft.ifft(fzdn, axis=0)
+    return ifzdp, ifzdn
+
 omega = 2.0
 omega2 = omega
 kz    = 2.0
@@ -37,65 +82,28 @@ y = A*np.cos(omega * tm - kz*zm) + B*np.cos(omega2 * tm + kz2*zm)
 up = A*np.cos(omega * tm - kz*zm)
 dn = B*np.cos(omega2 * tm + kz2*zm)
 
-## Step 1
+t_then_z = False
+if t_then_z == True:
+    ## Step 1
+    ift_t_y = FT_in_time(t, z, y, dt)
+    ### Step 2
+    ift_z_y_p, ift_z_y_n = FT_in_space(t, z, ift_t_y, dz)
+    # Get up and down fields as F = |mag_f| * exp(i*phi_f)
+    up_field = ift_z_y_p.real * np.exp(np.real(1j * ift_z_y_p.imag))
+    dn_field = ift_z_y_n.real * np.exp(np.real(1j * ift_z_y_n.imag))
+else:
+    ## Step 1
+    ift_z_y_p, ift_z_y_n = FT_in_space(t, z, y, dz)
+    ## Step 2
+    up_f = FT_in_time(t, z, ift_z_y_p, dt)
+    dn_f = FT_in_time(t, z, ift_z_y_n, dt)
+    # Get up and down fields as F = |mag_f| * exp(i*phi_f)
+    up_field = up_f.real * np.exp(np.real(1j * up_f.imag))
+    dn_field = dn_f.real * np.exp(np.real(1j * dn_f.imag))
 
-# fourier transform in time, filter negative freq's, inverse fourier transform
-def FT_in_time(t, z, data, dt):
-    # FT in time of the data (axis 1 is time)
-    ftd = np.fft.fft(data, axis=1)
-    # find relevant frequencies
-    freq = np.fft.fftfreq(len(t), dt)
-    f_grid, z_grid = np.meshgrid(freq, z)
-    # Filter out negative frequencies
-    for i in range(f_grid.shape[0]):
-        for j in range(f_grid.shape[1]):
-            if f_grid[i][j] < 0.0:
-                # Gets rid of negative freq's
-                ftd[i][j] = 0
-            else:
-                # Corrects for lost amplitude
-                ftd[i][j] = ftd[i][j] * 2.0
-    # inverse fourier transform in time of the data
-    iftd = np.fft.ifft(ftd, axis=1)
-    #   a complex valued signal where iftd.real == data, or close enough
-    return iftd
-
-ift_t_y = FT_in_time(t, z, y, dt)
-
-### Step 2
-
-# fourier transform in spatial dimension (z)
-#   similar to FT in time, but switch dimensions around
-def FT_in_space(t, z, data, dz):
-    # FT in space (z) of the data (axis 0 is z) for positive wave numbers
-    fzdp = np.fft.fft(data, axis=0)
-    # make a copy for the negative wave numbers
-    fzdn = fzdp.copy()
-    # find relevant wavenumbers
-    k_zs = np.fft.fftfreq(len(z), dz)
-    t_grid, k_grid = np.meshgrid(t, k_zs)
-    # Filter out one half of wavenumbers to separate up and down
-    for i in range(k_grid.shape[0]):
-        for j in range(k_grid.shape[1]):
-            if k_grid[i][j] > 0.0:
-                # for up, remove values for positive wave numbers
-                fzdp[i][j] = 0.0
-            else:
-                # for down, remove values for negative wave numbers
-                fzdn[i][j] = 0.0
-    # inverse fourier transform in space (z)
-    ifzdp = np.fft.ifft(fzdp, axis=0)
-    ifzdn = np.fft.ifft(fzdn, axis=0)
-    return ifzdp, ifzdn
-
-ift_z_y_p, ift_z_y_n = FT_in_space(t, z, ift_t_y, dz)
-
-# Get up and down fields as F = |mag_f| * exp(i*phi_f)
-up_field = ift_z_y_p.real * np.exp(np.real(1j * ift_z_y_p.imag))
-dn_field = ift_z_y_n.real * np.exp(np.real(1j * ift_z_y_n.imag))
 
 # plotting
-diff = False
+diff = True
 if diff == True:
     rows = 1
     cols = 3
